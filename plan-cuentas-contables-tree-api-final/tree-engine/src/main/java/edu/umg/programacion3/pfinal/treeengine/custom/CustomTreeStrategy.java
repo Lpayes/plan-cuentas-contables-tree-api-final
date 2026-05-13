3e1352a4e287f11373f5184843f0539eddeacc9a
Lester David Payes Méndez <lpayesm@miumg.edu.gt>
2026-05-13 14:28:40 -0600

    agregué esqueleto custom del motor del árbol

---
+++ plan-cuentas-contables-tree-api-final/tree-engine/src/main/java/edu/umg/programacion3/pfinal/treeengine/custom/CustomTreeStrategy.java
@@ -0,0 +1,21 @@
+package edu.umg.programacion3.pfinal.treeengine.custom;
+import edu.umg.programacion3.pfinal.treeengine.strategy.TreeAlgorithmStrategy;
+
+public class CustomTreeStrategy  implements TreeAlgorithmStrategy{
+
+	@Override
+	public void createRoot(String name) {
+	
+	}
+
+	@Override
+	public void addChild(Long parentId, String name) {
+		
+	}
+
+	@Override
+	public void printTree() {
+		
+	}
+
+}
