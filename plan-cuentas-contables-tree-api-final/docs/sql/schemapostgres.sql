fc0c2f10d9c9d19fa606a4dc4fa22acf68de583c
Jose Rosales <Jrosalesp16@miumg.edu.gt>
2026-05-14 14:02:59 -0600

    agregué docker compose y levanté postgres para el árbol

---
+++ plan-cuentas-contables-tree-api-final/docs/sql/schemapostgres.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS nodes (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    parent_id BIGINT,
+    CONSTRAINT fk_nodes_parent
+        FOREIGN KEY (parent_id)
+        REFERENCES nodes(id)
+);
