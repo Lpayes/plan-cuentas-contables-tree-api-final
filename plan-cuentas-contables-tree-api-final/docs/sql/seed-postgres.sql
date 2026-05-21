eba55de6f2924f27f0b85e287efe94acce861fdc
Jose Rosales <Jrosalesp16@miumg.edu.gt>
2026-05-20 23:06:06 -0600

    agregar datos de prueba PostgreSQL

--- plan-cuentas-contables-tree-api-final/docs/sql/seed-postgres.sql
+++ plan-cuentas-contables-tree-api-final/docs/sql/seed-postgres.sql
@@ -1,3 +1,5 @@
+DELETE FROM nodes;
+
 INSERT INTO nodes (name, parent_id) VALUES ('1 Activo', NULL);
 INSERT INTO nodes (name, parent_id) VALUES ('1.1 Caja', 1);
 INSERT INTO nodes (name, parent_id) VALUES ('1.1.1 Caja General', 2);
